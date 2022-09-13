71208abe32a267c8c2425ecb5755ffe300ce3fb3
endybv <andrei_wind@yahoo.com>
2022-09-13 08:00:05 +0200

    testing how it works

---
+++ test.js
@@ -0,0 +1,45 @@
+let home = document.getElementById("home-score")
+let one = 0
+
+
+function homeBy1() {
+    one += 1
+    home.textContent = one
+}
+
+function homeBy2() {
+    one += 2
+    home.textContent = one
+}
+
+function homeBy3() {
+    one += 3
+    home.textContent = one
+}
+
+
+let guest = document.getElementById("guest-score")
+let two = 0
+
+
+function guestBy1() {
+    two += 1
+    guest.textContent = two
+}
+
+function guestBy2() {
+    two += 2
+    guest.textContent = two
+}
+
+function guestBy3() {
+    two += 3
+    guest.textContent = two
+}
+
+function reset() {
+    one = 0
+    two = 0
+    home.textContent = one
+    guest.textContent = two
+}
